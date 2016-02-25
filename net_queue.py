5fb4dc2ea7d73869fec990871ddab1739c9de82a
Alien Lien <alien_lien@htc.com>
2016-02-25 18:52:54 +0800

    Add id for queue

--- net_queue.py
+++ net_queue.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import queue
+import uuid
 
 
 class NetQueue(queue.Queue):
 
-    def __init__(self, rate=0.0):
+    def __init__(self, id="", rate=0.0):
         self.rate = rate
+        if id == "":
+            self.id = uuid.uuid4()
         super(NetQueue, self).__init__()
 
     def __unicode__(self):
         max_size = q.maxsize if q.maxsize > 0 else 'Infinity'
-        return '[Queue - Rate: {rate}, Size: {size}, Max: {max_size}]' \
-            .format(rate=self.rate, size=self.qsize(), max_size=max_size)
+        return '[ID: {id}, Rate: {rate}, Size: {size}, Max: {max_size}]' \
+            .format(id=self.id, rate=self.rate,
+                size=self.qsize(), max_size=max_size)
 
     def __str__(self):
         return self.__unicode__()
