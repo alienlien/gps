91db6659faa4bdd1344abd2d867f86939e934e20
Alien Lien <alien_lien@htc.com>
2016-02-25 17:38:08 +0800

    Rename queue as net_queue

---
+++ net_queue.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import queue
+
+
+class NetQueue(queue.Queue):
+
+    def __init__(self, rate=0.0):
+        self.rate = rate
+        super(NetQueue, self).__init__()
+
+    def __unicode__(self):
+        max_size = q.maxsize if q.maxsize > 0 else 'Infinity'
+        return '[Queue - Rate: {rate}, Size: {size}, Max: {max_size}]' \
+            .format(rate=self.rate, size=self.qsize(), max_size=max_size)
+
+    def __str__(self):
+        return self.__unicode__()
+
+if __name__ == '__main__':
+    q = NetQueue(rate=0.5)
+    q.put('test')
+    q.put([1, 2, 3])
+    print(q.get())
+    print('Queue:', q)
